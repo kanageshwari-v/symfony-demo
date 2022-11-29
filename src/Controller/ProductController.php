2200386f6c9d6224cfd0b8de1452f7a1b3645308
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2022-11-29 07:10:40 +0000

    crud.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,45 +5,32 @@ namespace App\Controller;
 use App\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'index')]
+    #[Route('/products', name: 'index')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $entityManager = $doctrine->getManager();
+        $products = $doctrine->getRepository(Product::class)
+        ->findAll();
 
-        $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setDescription('Ergonomic and stylish!');
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'Lakshmi',
-            'id' => $product->getId(),
+            'products' => $products,
         ]);
     }
 
-    #[Route('/product/create', name: 'create_product')]
-    public function create(ManagerRegistry $doctrine): Response
+    #[Route('/products/create', name: 'create_product')]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $product = new Product();
-        // $product->setName('Keyboard');
-        // $product->setPrice(1999);
-        // $product->setDescription('Ergonomic and stylish!');
-
         $form = $this->createFormBuilder($product)
         ->add('name',TextType::class)
         ->add('price',NumberType::class)
@@ -55,7 +42,7 @@ class ProductController extends AbstractController
         if($form->isSubmitted()){
             $todo = $form->getData();
 
-            $en = $this->getDoctrine()->getManager();
+            $en = $doctrine->getManager();
             $en->persist($product);
             $en->flush();
 
@@ -67,5 +54,20 @@ class ProductController extends AbstractController
             'form'=> $form->createView()
         ]);
 
+    }
+
+    #[Route('/product/{id}', name: 'delete_product')]
+    public function delete(ManagerRegistry $doctrine, int $id): Response
+    {
+        $product = $doctrine->getRepository(Product::class)->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        $em = $doctrine->getManager();
+        $em->remove($product);
+        $em->flush();
+        return $this->redirectToRoute('index');
     }
 }
