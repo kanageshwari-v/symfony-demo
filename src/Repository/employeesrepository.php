abb7724e333240253cc61838b42e1bf1bb3761c0
kanageshwari-v <kanageshwari10@gmail.com>
2022-12-01 18:27:57 +0000

    third  commit

--- src/Repository/employeesrepository.php
+++ src/Repository/employeesrepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\employees;
+use App\Entity\employee;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
